f3864840e1d5239540967fdc65eeb834636bf775
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-05-09 23:36:11 +0700

    Menambahkan Automata, Table-driven construction, Parameterization/generics, dan Code reuse/library

--- JabbarTransLibraries/Class1.cs
+++ JabbarTransLibraries/Class1.cs
@@ -35,10 +35,73 @@ namespace JabbarTransLibraries
             DIBERANGKATKAN
         }
 
+        public enum Trigger
+        {
+            PILIH_TUJUAN,
+            CEK_HARGA,
+            BERANGKAT
+        }
+
         public class ProsesPemesanan
         {
             public prosesPesan currentState = prosesPesan.ASAL;
 
+            public class Transition {
+                public prosesPesan stateAwal;
+                public prosesPesan stateAkhir;
+                public Trigger trigger;
+
+                public Transition(prosesPesan stateAwal, prosesPesan stateAkhir, Trigger trigger)
+                {
+                    this.stateAwal = stateAwal;
+                    this.stateAkhir = stateAkhir;
+                    this.trigger = trigger;
+                }
+            }
+
+            Transition[] transisi =
+            {
+                new Transition(prosesPesan.ASAL, prosesPesan.TUJUAN, Trigger.PILIH_TUJUAN),
+                new Transition(prosesPesan.TUJUAN, prosesPesan.HARGA, Trigger.CEK_HARGA),
+                new Transition(prosesPesan.HARGA, prosesPesan.DIBERANGKATKAN, Trigger.BERANGKAT)
+            };
+
+            public prosesPesan getStateBerikutnya(prosesPesan stateAwal, Trigger trigger) {
+                prosesPesan stateAkhir = stateAwal;
+
+                for (int i = 0; i < transisi.Length; i++)
+                {
+                    Transition perubahan = transisi[i];
+
+                    if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                    {
+                        stateAkhir = perubahan.stateAkhir;
+                    }
+                }
+                currentState = stateAkhir;
+                return currentState;
+            }
+
+            public void activateTrigger(Trigger trigger) {
+                currentState = getStateBerikutnya(currentState, trigger);
+                Console.WriteLine(currentState);
+
+                if (currentState == prosesPesan.ASAL)
+                {
+                    Console.WriteLine("Asal keberangkatan");
+                }
+                else if (currentState == prosesPesan.TUJUAN)
+                {
+                    Console.WriteLine("Pilih tujuan keberangkatan");
+                }
+                else if (currentState == prosesPesan.HARGA)
+                {
+                    Console.WriteLine("Check harga");
+                }
+                else {
+                    Console.WriteLine("Berangkat");
+                }
+            }
 
             public void printCurrentState()
             {
@@ -48,11 +111,8 @@ namespace JabbarTransLibraries
 
             public AreaType pilihAsal(int choice)
             {
-                /* Debug.Assert(currentState == prosesPesan.ASAL, "Maaf, Anda hanya dapat memilih asal saat state berada di ASAL");*/
-                /*if (currentState != prosesPesan.ASAL)
-                {
-                    Console.WriteLine("Maaf, method ini hanya dapat diakses saat state berada di ASAL");
-                }*/
+                //Debug.Assert(currentState == prosesPesan.ASAL, "Maaf, Anda hanya dapat memilih asal saat state berada di ASAL");
+                
 
                 Debug.Assert(choice != null && choice <= 2, "input tidak valid!");
 
@@ -62,47 +122,32 @@ namespace JabbarTransLibraries
                 {
                     // Menggunakan enum Bandung
                     selectedEnum = AreaType.Bandung; // Ubah Tasik ke enum yang diinginkan
-                    /*Console.WriteLine($"Kantor Pemberangkatan: Bandung");*/
-
-                    currentState = prosesPesan.TUJUAN;
+                    
                 }
                 else if (choice == 2)
                 {
                     // Menggunakan enum Jakarta
                     selectedEnum = AreaType.Jakarta; // Ubah Tasik ke enum yang diinginkan
-                    /*Console.WriteLine($"Kantor Pemberangkatan: Jakarta");*/
-
-                    currentState = prosesPesan.TUJUAN;
+                    
                 }
                 return selectedEnum;
             }
-
-            public T pilihTujuan<T>(int choice) where T : Enum
+            
+            public T pilihTujuan<T>(int choice, int choiceTujuan) where T : Enum
             {
-                Debug.Assert(currentState == prosesPesan.TUJUAN, "Maaf, method ini hanya dapat diakses saat state berada di TUJUAN");
+                //Debug.Assert(currentState == prosesPesan.TUJUAN, "Maaf, method ini hanya dapat diakses saat state berada di TUJUAN");
 
                 AreaType kantorAsal = pilihAsal(choice);
-
-
-                currentState = prosesPesan.HARGA;
 
                 if (kantorAsal == AreaType.Bandung)
                 {
                     // Menggunakan enum Bandung
-                    Console.WriteLine("Trayek tersedia di kantor Bandung:");
-                    Console.WriteLine("1. Tasik");
-                    Console.WriteLine("2. Cilacap");
-                    Console.WriteLine("3. Magelang");
-                    Console.WriteLine("4. Yogya");
-                    Console.WriteLine("5. Wonogiri");
-                    Console.WriteLine("6. Pacitan");
+                    
 
-                    int bandungChoice = int.Parse(Console.ReadLine());
-
-                    Debug.Assert(bandungChoice != null && bandungChoice <= 6, "input tidak valid!");
+                    Debug.Assert(choiceTujuan != null && choiceTujuan <= 6, "input tidak valid!");
                     Bandung bandungEnum;
 
-                    switch (bandungChoice)
+                    switch (choiceTujuan)
                     {
                         case 1:
                             bandungEnum = Bandung.Tasik;
@@ -131,17 +176,12 @@ namespace JabbarTransLibraries
                 else if (kantorAsal == AreaType.Jakarta)
                 {
                     // Menggunakan enum Jakarta
-                    Console.WriteLine("Trayek tersedia di kantor Jakarta:");
-                    Console.WriteLine("1. Tasik");
-                    Console.WriteLine("2. Banjar");
-                    Console.WriteLine("3. Pangandaran");
+                   
 
-                    int jakartaChoice = int.Parse(Console.ReadLine());
-
-                    Debug.Assert(jakartaChoice != null && jakartaChoice <= 3, "input tidak valid!");
+                    Debug.Assert(choiceTujuan != null && choiceTujuan <= 3, "input tidak valid!");
                     Jakarta jakartaEnum;
 
-                    switch (jakartaChoice)
+                    switch (choiceTujuan)
                     {
                         case 1:
                             jakartaEnum = Jakarta.Tasik;
@@ -161,6 +201,61 @@ namespace JabbarTransLibraries
                 else
                 {
                     throw new ArgumentException("Kantor asal tidak valid!");
+                }
+            }
+
+            public void cekHarga(int choice, int tujuanChoice)
+            {
+                Debug.Assert(currentState == prosesPesan.HARGA, "Maaf, method ini hanya dapat diakses saat state berada di HARGA");
+
+                AreaType kantorAsal = pilihAsal(choice);
+
+                if (kantorAsal == AreaType.Bandung)
+                {
+                    Bandung asalBandung = pilihTujuan<Bandung>(choice, tujuanChoice);
+
+                    switch (asalBandung)
+                    {
+                        case Bandung.Tasik:
+                            Console.WriteLine("Harga tiket Bandung - Tasik sebesar Rp. 100.000");
+                            break;
+                        case Bandung.Cilacap:
+                            Console.WriteLine("Harga tiket Bandung - Cilacap sebesar Rp. 120.000");
+                            break;
+                        case Bandung.Magelang:
+                            Console.WriteLine("Harga tiket Bandung - Magelang sebesar Rp. 140.000");
+                            break;
+                        case Bandung.Yogya:
+                            Console.WriteLine("Harga tiket Bandung - Yogya sebesar Rp. 160.000");
+                            break;
+                        case Bandung.Wonogiri:
+                            Console.WriteLine("Harga tiket Bandung - Wonogiri sebesar Rp. 180.000");
+                            break;
+                        case Bandung.Pacitan:
+                            Console.WriteLine("Harga tiket Bandung - Pacitan sebesar Rp. 200.000");
+                            break;
+                        default:
+                            throw new ArgumentException("Tujuan tidak valid!");
+                    }
+                }
+                else if (kantorAsal == AreaType.Jakarta)
+                {
+                    Jakarta asalJakarta = pilihTujuan<Jakarta>(choice, tujuanChoice);
+
+                    switch (asalJakarta)
+                    {
+                        case Jakarta.Tasik:
+                            Console.WriteLine("Harga tiket Jakarta - Tasik sebesar Rp. 100.000");
+                            break;
+                        case Jakarta.Banjar:
+                            Console.WriteLine("Harga tiket Jakarta - Banjar sebesar Rp. 120.000");
+                            break;
+                        case Jakarta.Pangandaran:
+                            Console.WriteLine("Harga tiket Jakarta - Pangadaran sebesar Rp. 140.000");
+                            break;
+                        default:
+                            throw new ArgumentException("Tujuan tidak valid");
+                    }
                 }
             }
         }
--- mainProgram/Program.cs
+++ mainProgram/Program.cs
@@ -10,19 +10,105 @@ namespace JabbarTransLibraries
         {
             ProsesPemesanan pesan = new ProsesPemesanan();
 
-            Console.WriteLine("Pilih kantor pemberangkatan:");
+            Console.WriteLine("Pilih tempat keberangkatan:");
             Console.WriteLine("1. Bandung");
             Console.WriteLine("2. Jakarta");
 
             int choice = int.Parse(Console.ReadLine());
             // Pilih kantor pemberangkatan
-            Console.WriteLine($"Anda memilih kantor pemberangkatan: {pesan.pilihAsal(choice)}");
+            pesan.pilihAsal(choice);
+            Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+            string keyakinan = Console.ReadLine();
 
-            // Pilih tujuan
-            Console.WriteLine($"Anda memilih kota tujuan: {pesan.pilihTujuan<Enum>(choice)}");
+            //Debug.Assert(keyakinan != null || keyakinan != )
 
-            // Cek harga
-            pesan.printCurrentState();
+            if (keyakinan == "Y" || keyakinan == "y") {
+                pesan.getStateBerikutnya(prosesPesan.ASAL, Trigger.PILIH_TUJUAN);
+            } else if (keyakinan == "N" || keyakinan == "n") 
+            {
+                while (keyakinan != "y" && keyakinan != "Y")
+                {
+                    Console.WriteLine("Pilih tempat keberangkatan:");
+                    choice = int.Parse(Console.ReadLine());
+                    pesan.pilihAsal(choice);
+                    Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+                    keyakinan = Console.ReadLine();
+                }
+
+                pesan.getStateBerikutnya(prosesPesan.ASAL, Trigger.PILIH_TUJUAN);
+            }
+
+            if(pesan.pilihAsal(choice) == AreaType.Bandung)
+            {
+                Console.WriteLine("Trayek tersedia di kantor Bandung:");
+                Console.WriteLine("1. Tasik");
+                Console.WriteLine("2. Cilacap");
+                Console.WriteLine("3. Magelang");
+                Console.WriteLine("4. Yogya");
+                Console.WriteLine("5. Wonogiri");
+                Console.WriteLine("6. Pacitan");
+
+                Console.WriteLine("Pilih kota tujuan");
+                int bandungChoice = int.Parse(Console.ReadLine());
+
+                pesan.pilihTujuan<Enum>(choice, bandungChoice);
+
+                Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+                keyakinan = Console.ReadLine();
+
+                if (keyakinan == "Y" || keyakinan == "y")
+                {
+                    pesan.getStateBerikutnya(prosesPesan.TUJUAN, Trigger.CEK_HARGA);
+                }
+                else if (keyakinan == "N" || keyakinan == "n")
+                {
+                    while (keyakinan != "y" && keyakinan != "Y") 
+                    {
+                        Console.WriteLine("Pilih kota tujuan");
+                        bandungChoice = int.Parse(Console.ReadLine());
+                        pesan.pilihTujuan<Enum>(choice, bandungChoice);
+                        Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+                        keyakinan = Console.ReadLine();
+                    }
+
+                    pesan.getStateBerikutnya(prosesPesan.TUJUAN, Trigger.CEK_HARGA);
+                
+                }
+                pesan.cekHarga(choice, bandungChoice);
+            } else if (pesan.pilihAsal(choice) == AreaType.Jakarta)
+            {
+                Console.WriteLine("Trayek tersedia di kantor Jakarta:");
+                Console.WriteLine("1. Tasik");
+                Console.WriteLine("2. Banjar");
+                Console.WriteLine("3. Pangandaran");
+
+                Console.WriteLine("Pilih kota tujuan");
+                int jakartaChoice = int.Parse(Console.ReadLine());
+                pesan.pilihTujuan<Enum>(choice, jakartaChoice);
+
+                Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+                keyakinan = Console.ReadLine();
+
+                if (keyakinan == "Y" || keyakinan == "y")
+                {
+                    pesan.getStateBerikutnya(prosesPesan.TUJUAN, Trigger.CEK_HARGA);
+                }
+                else if (keyakinan == "N" || keyakinan == "n")
+                {
+                    while(keyakinan != "y" && keyakinan != "Y")
+                    {
+                        Console.WriteLine("Pilih kota tujuan");
+                        jakartaChoice = int.Parse(Console.ReadLine());
+                        pesan.pilihTujuan<Enum>(choice, jakartaChoice);
+                        Console.WriteLine("Apakah anda yakin dengan pilihan anda? Sebaiknya jangan terlalu gegabah (Y / N)");
+                        keyakinan = Console.ReadLine();
+                    } 
+
+                    pesan.getStateBerikutnya(prosesPesan.TUJUAN, Trigger.CEK_HARGA);
+                 
+                }
+                pesan.cekHarga(choice, jakartaChoice);
+            }
         }
     }
 }
